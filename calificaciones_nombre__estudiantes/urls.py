30bf0f840c5aed51739180f428fc0dff65384652
Santiago25-lab <edwin.urbina@campusucc.edu.co>
2026-04-30 10:11:36 -0500

    subo dev4

--- calificaciones_nombre__estudiantes/urls.py
+++ calificaciones_nombre__estudiantes/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'calificaciones'
 
 urlpatterns = [
+    # Auth
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='calificaciones:login'), name='logout'),
+    path('registro/', views.registro, name='registro'),
+
+    # CRUD
     path('crear/', views.crear_calificacion, name='crear_calificacion'),
     path('', views.listar_calificaciones, name='listar_calificaciones'),
     path('editar/<int:pk>/', views.editar_calificacion, name='editar_calificacion'),
